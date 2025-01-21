309a9f0f0fa17c579ed66a8f0d6db8d689a47340
Itzik Hanan <itzikh02@gmail.com>
2025-01-22 01:07:39 +0200

    added quote function

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-
 from dotenv import load_dotenv
-import os
+import os, random, json
+
+# source .venv/bin/activate - to activate the virtual environment
 
 # Load environment variables from .env
 load_dotenv()
@@ -18,10 +19,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_name = update.effective_user.first_name
     await update.message.reply_text(f"Hello, {user_name}! How can I help you?")
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Respond to the /hello command."""
-    user_name = update.effective_user.first_name
-    await update.message.reply_text(f"Hello, {user_name}!")
+async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Respond to the /quote command."""
+    with open("quotes.json", "r") as file:
+        quotes = json.load(file)
+        quote = random.choice(quotes) 
+    await update.message.reply_text(f"{quote['quote']}\n\n_{quote['name']}_", parse_mode="Markdown")
 
 # Main function to set up the bot
 def main():
@@ -30,7 +33,7 @@ def main():
 
     # Add command handlers
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("hello", hello))
+    application.add_handler(CommandHandler("quote", quote))
 
     # Start polling for updates
     application.run_polling()
